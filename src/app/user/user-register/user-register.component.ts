5f92ec992194f18cdc37120ab37d94e185ce7ab9
DEVEN <devenparmar8502@gmail.com>
2024-03-08 19:21:11 +0530

    08032024 0721pm

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -9,22 +9,35 @@ import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators }
 export class UserRegisterComponent {
   registrationForm!: FormGroup;
   emailregex = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$';
-  constructor(){}
+  constructor(private fb:FormBuilder){}
   ngOnInit(){
-    this.registrationForm = new FormGroup({
-      name : new FormControl('',Validators.required),
-      // email : new FormControl('',[Validators.required,Validators.pattern(this.emailregex)]),
-      email : new FormControl('',Validators.required),
-      mobile : new FormControl('',Validators.required),
-      pass : new FormControl('',Validators.required),
-      confpass : new FormControl('',Validators.required)
-    },this.passwordMatchingValidator);
+  //   this.registrationForm = new FormGroup({
+  //     name : new FormControl('',Validators.required),
+  //     // email : new FormControl('',[Validators.required,Validators.pattern(this.emailregex)]),
+  //     email : new FormControl('',Validators.required),
+  //     mobile : new FormControl('',Validators.required),
+  //     pass : new FormControl('',Validators.required),
+  //     confpass : new FormControl('',Validators.required)
+  //   },this.passwordMatchingValidator);
+  this.createRegistrationForm();
   }
   onSubmit(){
     console.log(this.registrationForm.value);
   }
   get fget(){
     return this.registrationForm.controls;
+  }
+  createRegistrationForm(){
+    this.registrationForm = this.fb.group
+    ({
+      name : ['',Validators.required],
+      email : ['',Validators.required],
+      mobile : ['',Validators.required],
+      pass : ['',Validators.required],
+      confpass : ['',Validators.required]
+    },
+    {Validators:this.passwordMatchingValidator}
+    );
   }
   passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null {
     return fc.get('pass')?.value === fc.get('confpass')?.value ? null :
